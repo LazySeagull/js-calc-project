2e5ace749550edde6b5b523ec1fe8e3ea613e1c2
Ateeksh <ateesood@gmail.com>
2025-08-06 19:28:55 +0000

    bug fixing and optimization

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 // variable declared
-
-const one = document.getElementById("one");
-const two = document.getElementById("two");
-const three = document.getElementById("three");
-const four = document.getElementById("four");
-const five = document.getElementById("five");
-const six = document.getElementById("six");
-const seven = document.getElementById("seven");
-const eight = document.getElementById("eight");
-const nine = document.getElementById("nine");
-const zero = document.getElementById("zero");
+const numbers = document.querySelectorAll(".NumberButton #num")
 
 //operations
 
@@ -20,126 +10,77 @@ const subtract = document.getElementById("subtract");
 
 //other
 
-const equals = document.getElementById("equals");  
+const equals = document.getElementById("equals");
 const clear = document.getElementById("clear");
 
-// the display window 
+// the display window
 
 const equationReview = document.querySelector(".EquationReview");
-equationReview.textContent="";
+equationReview.textContent = "";
 const equationMain = document.querySelector(".EquationMain");
 equationMain.textContent = "";
 
 // append to the main string (numbers)
-one.addEventListener("click",()=>{
-    equationReview.textContent += one.textContent;
-    equationMain.textContent = one.textContent;
-})
 
-two.addEventListener("click",()=>{
-    equationReview.textContent += two.textContent;
-    equationMain.textContent = two.textContent;
+numbers.forEach(button =>{
+    button.addEventListener("click" , () =>{
+        equationReview.textContent += button.textContent;
+        equationMain.textContent = button.textContent;
+    })
 })
-
-three.addEventListener("click",()=>{
-    equationReview.textContent += three.textContent;
-    equationMain.textContent = three.textContent;
-})
-
-four.addEventListener("click",()=>{
-    equationReview.textContent += four.textContent;
-    equationMain.textContent = four.textContent;
-})
-
-five.addEventListener("click",()=>{
-    equationReview.textContent += five.textContent;
-    equationMain.textContent = five.textContent;
-})
-
-six.addEventListener("click",()=>{
-    equationReview.textContent += six.textContent;
-    equationMain.textContent = six.textContent;
-})
-
-seven.addEventListener("click",()=>{
-    equationReview.textContent += seven.textContent;
-    equationMain.textContent = seven.textContent;
-})
-
-eight.addEventListener("click",()=>{
-    equationReview.textContent += eight.textContent;
-    equationMain.textContent = eight.textContent;
-})
-
-nine.addEventListener("click",()=>{
-    equationReview.textContent += nine.textContent;
-    equationMain.textContent = nine.textContent;
-})
-
-zero.addEventListener("click",()=>{
-    equationReview.textContent += zero.textContent;
-    equationMain.textContent = zero.textContent;
-})
-
 
 // add operations to the main string
 
-add.addEventListener("click" , () => {
+add.addEventListener("click", () => {
+  if (equationReview.textContent == "" && equationMain.textContent != "") {
+    equationReview.textContent = equationMain.textContent + add.textContent;
+    equationMain.textContent = add.textContent;
+  } else {
+    equationMain.textContent = add.textContent;
+    equationReview.textContent += add.textContent;
+  }
+});
 
-    if(equationReview.textContent == "" && equationMain.textContent != ""){
-        equationReview.textContent = equationMain.textContent + add.textContent;
-        equationMain.textContent = add.textContent;
-    }
-    else{
-        equationMain.textContent = add.textContent;
-        equationReview.textContent += add.textContent;
-    }
-})
+mul.addEventListener("click", () => {
+  if (equationReview.textContent == "" && equationMain.textContent != "") {
+    equationReview.textContent = equationMain.textContent + mul.textContent;
+    equationMain.textContent = mul.textContent;
+  } else {
+    equationMain.textContent = mul.textContent;
+    equationReview.textContent += mul.textContent;
+  }
+});
 
-mul.addEventListener("click" , () => {
-     if(equationReview.textContent == "" && equationMain.textContent != ""){
-        equationReview.textContent = equationMain.textContent + mul.textContent;
-        equationMain.textContent = mul.textContent;
-    }
-    else{
-        equationMain.textContent = mul.textContent;
-        equationReview.textContent += mul.textContent;
-    }
-})
+divide.addEventListener("click", () => {
+  if (equationReview.textContent == "" && equationMain.textContent != "") {
+    equationReview.textContent = equationMain.textContent + divide.textContent;
+    equationMain.textContent = divide.textContent;
+  } else {
+    equationMain.textContent = divide.textContent;
+    equationReview.textContent += divide.textContent;
+  }
+});
 
-divide.addEventListener("click" , () => {
- if(equationReview.textContent == "" && equationMain.textContent != ""){
-        equationReview.textContent = equationMain.textContent + divide.textContent;
-        equationMain.textContent = divide.textContent;
-    }
-    else{
-        equationMain.textContent = divide.textContent;
-        equationReview.textContent += divide.textContent;
-    }
-})
+subtract.addEventListener("click", () => {
+  if (equationReview.textContent == "" && equationMain.textContent != "") {
+    equationReview.textContent =
+      equationMain.textContent + subtract.textContent;
+    equationMain.textContent = subtract.textContent;
+  } else {
+    equationMain.textContent = subtract.textContent;
+    equationReview.textContent += subtract.textContent;
+  }
+});
 
-subtract.addEventListener("click" , () => {
- if(equationReview.textContent == "" && equationMain.textContent != ""){
-        equationReview  .textContent = equationMainz.textContent + subtract.textContent;
-        equationMain.textContent = subtract.textContent;
-    }
-    else{
-        equationMain.textContent = subtract.textContent;
-        equationReview.textContent += subtract.textContent;
-    }
-})
+// other functions
 
+equals.addEventListener("click", () => {
+  const result = math.evaluate(equationReview.textContent);
+  equationMain.textContent = result;
+  equationReview.textContent = "";
+});
 
-// other functions 
-
-equals.addEventListener("click" , () =>{
-    const result = math.evaluate(equationReview.textContent);
-    equationMain.textContent = result;
-    equationReview.textContent= "";
-})
-
-
-clear.addEventListener("click" , () => {
-    equationMain.textContent ="";
-    equationReview.textContent="";
-})
+clear.addEventListener("click", () => {
+  equationMain.textContent = "";
+  equationReview.textContent = "";
+});
